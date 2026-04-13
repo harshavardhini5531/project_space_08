a7483a5ab26b886d94df97d78591fb4bbe374f31
harshavardhini5531 <harshavardhini.j@adityauniversity.in>
2026-04-13 14:07:22 +0530

    project status tables3

--- app/layout.js
+++ app/layout.js
@@ -18,6 +18,8 @@ export default function RootLayout({ children }) {
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         <link href="https://fonts.googleapis.com/css2?family=DM+Sans:ital,opsz,wght@0,9..40,300;0,9..40,400;0,9..40,500;0,9..40,600;0,9..40,700;0,9..40,800;1,9..40,300;1,9..40,400&family=Orbitron:wght@400;500;600;700;800;900&display=swap" rel="stylesheet" />
         <link href="https://fonts.cdnfonts.com/css/astro-futuristic-font" rel="stylesheet" />
+        <link rel="manifest" href="/manifest.json" />
+        <meta name="theme-color" content="#fd1c00" />
       </head>
       <body suppressHydrationWarning>
         <ConsoleGuard />
